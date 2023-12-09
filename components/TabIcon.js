d547730ec742085199f2d4d8bc06200def09fc7b
Sebastian <iwan.sebastian029@gmail.com>
2023-12-09 23:31:17 +0100

    tabNav icons and focused colors

---
+++ components/TabIcon.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+const TabIcon = ({ routeName, focused }) => {
+  const iconColor = focused ? 'orange' : 'black'; // Change color to orange when focused
+
+  switch (routeName) {
+    case 'Home':
+      return <AntDesign name="home" style={[styles.icon, { color: iconColor }]} />;
+    case 'Explore':
+      return <AntDesign name="search1" style={[styles.icon, { color: iconColor }]} />;
+    case 'Cart':
+      return <AntDesign name="shoppingcart" style={[styles.icon, { color: iconColor }]} />;
+    case 'Account':
+      return <AntDesign name="user" style={[styles.icon, { color: iconColor }]} />;
+    default:
+      return <AntDesign name="unknowfile1" style={[styles.icon, { color: iconColor }]} />;
+  }
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 20,
+  },
+});
+
+export default TabIcon;
--- navigation/Tab.js
+++ navigation/Tab.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../views/Home/home-screen';
 import DrawerNav from "./Drawer";
+import TabIcon from '../components/TabIcon' 
 
 const optionScreen = {
     headerShown: false,
@@ -13,10 +14,15 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNav() {
     return (
-        <Tab.Navigator screenOptions={({ route }) => ({})}>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, }) => {
+                return <TabIcon routeName={route?.name} focused={focused} />;
+            }
+        })}>
             <Tab.Screen name="Home" component={HomeScreen} options={optionScreen} />
             <Tab.Screen name="Explore" component={HomeScreen} options={optionScreen} />
             <Tab.Screen name="Cart" component={HomeScreen} options={optionScreen} />
+            <Tab.Screen name="Account" component={HomeScreen} options={optionScreen} />
             <Tab.Screen name="DrawerNav" component={DrawerNav} options={optionScreen} />
             
         </Tab.Navigator>
