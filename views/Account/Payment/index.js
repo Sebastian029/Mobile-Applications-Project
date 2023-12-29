e713b54a53f59fc4b9b9c251df5b2dd3e6f57fa0
codd <102369774+jj0rd@users.noreply.github.com>
2023-12-29 18:31:21 +0100

    card dodaj/usun

--- views/Account/Payment/index.js
+++ views/Account/Payment/index.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, Pressable, FlatList } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './style';
+import { baseUrl } from '../../../config';
+import axios from 'axios';
 
 const PaymentScreen = ({ navigation }) => {
   const [cardData, setCardData] = useState([]);
@@ -52,22 +54,44 @@ const PaymentScreen = ({ navigation }) => {
     </Pressable>
   );
 
-  const handleSaveCard = (newCard) => {
+  const handleSaveCard = async (newCard) => {
     // Dodaj nową kartę do stanu
     setCardData([...cardData, newCard]);
+    // Zapisz dane karty do AsyncStorage
+    saveCardDataToStorage([...cardData, newCard]);
+
+    try {
+      const storedUserData = await AsyncStorage.getItem('userData');
+      const parsedUserData = JSON.parse(storedUserData);
+      if (parsedUserData && parsedUserData.id) {
+        const userId = parsedUserData.id;
+        // Dodaj nową kartę do bazy danych
+        await axios.post(`${baseUrl}/cardData`, { ...newCard, userid: userId });
+      } else {
+        console.error("Error reading user data from AsyncStorage");
+      }
+    } catch (error) {
+      console.error('Error updating data in the database:', error);
+    }
   };
 
-  const handleDeleteCard = (cardToDelete) => {
-    // Znajdź indeks karty do usunięcia
+  const handleDeleteCard = async (cardToDelete) => {
     const index = cardData.findIndex((card) => card.number === cardToDelete.number);
 
     if (index !== -1) {
-      // Utwórz kopię stanu i usuń kartę
       const updatedCardData = [...cardData];
       updatedCardData.splice(index, 1);
 
-      // Uaktualnij stan
-      setCardData(updatedCardData);
+      try {
+        // Usuń dane z bazy danych
+        await axios.delete(`${baseUrl}/cardData/${cardToDelete.id}`);
+
+        // Aktualizuj stan i zapisz dane karty do AsyncStorage
+        setCardData(updatedCardData);
+        saveCardDataToStorage(updatedCardData);
+      } catch (error) {
+        console.error('Error deleting data from the database:', error);
+      }
     }
   };
 
